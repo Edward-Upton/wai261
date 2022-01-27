5cc401b1c527a212e4c89dc4ce10892666fd5eec
Edward Upton <ed.upton@outlook.com>
2022-01-27 19:06:21 +0000

    Created simple ChakraUI UI

---
+++ src/Message.tsx
@@ -0,0 +1,11 @@
+import { Box, Text } from "@chakra-ui/react";
+
+function Message() {
+  return (
+    <Box backgroundColor="black">
+      <Text color="white">Message</Text>
+    </Box>
+  );
+}
+
+export default Message;
